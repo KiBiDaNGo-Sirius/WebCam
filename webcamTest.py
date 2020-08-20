5133a024de5242b9ba8e7e556ab59753a6d8f0cb
KiBiDaNGo-sirius <shunsuke.sirius@gmail.com>
2020-08-21 00:02:55 +0900

    HowToMakeDynamicText

--- webcamTest.py
+++ webcamTest.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import cv2
 from PIL import Image,ImageTk
 import numpy as np
+import threading
 
 #Main window
 window=tk.Tk()
@@ -14,7 +15,8 @@ Vcanvas.place(x=0, y=0)
 #text canvas
 Tcanvas = tk.Canvas(window, width=200, height=450, bg="blue")
 Tcanvas.place(x=600, y=0)
-
+#text list
+textlist = ["おはよう","こんにちは","こんばんは"]
 
 def capStart():
     print('camera-ON')
@@ -44,8 +46,15 @@ def update():#update
         print("u-Fail")
     window.after(1,update)
 
+def text():
+    global textlist
+    for i in range(len(textlist)):
+        Tcanvas.create_text(100, 20*(i+1), text=textlist[i], font=("Meiryo", 18, "bold"))
+    
+
 capStart()
 update()
+text()
 window.mainloop()
 
 #参考にさせていただいたページ　https://shizenkarasuzon.hatenablog.com/entry/2018/12/31/064646　https://teratail.com/questions/187773
